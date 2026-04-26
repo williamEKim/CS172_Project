0e3d26f07ef73c3585b3b07a4300e4540f4b6711
williamEkim <dongyeon0308@gmail.com>
2026-04-25 23:12:08 -0700

    formatted the main.py to print a sample post after collection

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Bluesky import login, fetch_posts
 from getpass import getpass
 
-TARGET_SIZE = 500 * 1024 * 1024   # 500MB
+# TARGET_SIZE = 500 * 1024 * 1024   # 500MB
+TARGET_SIZE = 3 * 1024 * 1024      # set it to 3MB for now (testing purpose)
 FILE_SIZE_LIMIT = 10 * 1024 * 1024  # 10MB per file
 
 def main():
@@ -15,6 +16,14 @@ def main():
     query = input("Enter search term: ")
 
     posts = fetch_posts(token, query, TARGET_SIZE)
+
+    collected = list(posts)
+
+    print("Data Collection is completed:")
+    print(f"Data collection completed: {len(collected)} posts fetched")
+    print("\nSample post:")
+    print(collected[0])
+
     # process_posts() # needs to be implemented
 
 if __name__ == "__main__":
