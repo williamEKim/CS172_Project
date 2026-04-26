8c857f31840022eae2514f9d6441351739b8232b
williamEkim <dongyeon0308@gmail.com>
2026-04-25 22:53:40 -0700

    updated the password entering to be private

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from Bluesky import login, fetch_posts
+from getpass import getpass
 
 TARGET_SIZE = 500 * 1024 * 1024   # 500MB
 FILE_SIZE_LIMIT = 10 * 1024 * 1024  # 10MB per file
 
 def main():
     handle = input("Enter your Bluesky handle (e.g. yourname.bsky.social): ")
-    password = input("Enter your Bluesky app password: ")
+    password = getpass("Enter your Bluesky app password: ")
 
     token = login(handle, password)
     if not token:
