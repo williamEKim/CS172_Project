898519e1d260660268adeb625f79c088c597088f
TiffanyTrinh09 <ttrin046@ucr.edu>
2026-04-30 23:20:45 -0700

    updated main.py on top of process_post() and save_posts()

--- main.py
+++ main.py
@@ -1,57 +1,40 @@
 from Bluesky import login, fetch_posts, extract_urls, fetch_titles
+from process import process_post, save_posts
 from getpass import getpass
-import os, json
 
-# TARGET_SIZE = 500 * 1024 * 1024   # 500MB
-TARGET_SIZE = 3 * 1024 * 1024      # set it to 3MB for now (testing purpose)
-# FILE_SIZE_LIMIT = 10 * 1024 * 1024  # 10MB per file
-FILE_SIZE_LIMIT = 3 * 1024 * 1024  # 3MB per file
-OUTPUT_DIR = "./raw_files"
+TARGET_SIZE = 500 * 1024 * 1024
+FILE_SIZE_LIMIT = 10 * 1024 * 1024
+
+QUERIES = [
+    "baseball", "football", "hockey", "volleyball", "golf", "cricket", "rugby", "skiing", "snowboarding", "tennis", "boxing", "mma", "wrestling", "cycling",
+    "chess", "esports", "darts", "badminton", "pool", "ice skating", "rowing", "surfing"
+]
 
 def main():
     handle = input("Enter your Bluesky handle (e.g. yourname.bsky.social): ")
     password = getpass("Enter your Bluesky app password: ")
-    
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    file_index = 1
-    file_size = 0
-    
+
     token = login(handle, password)
     if not token:
         return
 
-    query = input("Enter search term: ")
+    output_dir = input("Enter output directory: ")
 
-    file = open(f"{OUTPUT_DIR}/bluesky_{query}_{file_index}.jsonl", "w")
+    processed = []
 
-    posts = fetch_posts(token, query, TARGET_SIZE)
+    for query in QUERIES:
+        print(f"\nStarting query: {query}")
+        raw_posts = fetch_posts(token, query, TARGET_SIZE)
 
-    for post in posts:
-        extract_urls(post)
-        fetch_titles(post)
-
-        line = json.dumps(post) + "\n"
-        line_size = len(line.encode("utf-8"))
+        for post in raw_posts:
+            extract_urls(post)
+            processed.append(process_post(post))
         
-        if file_size + line_size > FILE_SIZE_LIMIT: # currently 3MB for testing
-            file.close()
-            file_size = 0
-            file_index += 1
-            file = open(f"{OUTPUT_DIR}/bluesky_{query}_{file_index}.jsonl", "w")
+    fetch_titles(processed)
+    save_posts(processed, output_dir, query)
 
-        file.write(line)
-        file_size += line_size
-            
-    file.close()
-
-    # collected = list(posts)
-
-    print("Data Collection is completed:")
-    # print(f"Data collection completed: {len(collected)} posts fetched")
-    # print("\nSample post:")
-    # print(collected[0])
-
-    # process_posts() # needs to be implemented
+    print(f"Finished query: {query}")
+    print("\nAll queries done.")
 
 if __name__ == "__main__":
     main()
